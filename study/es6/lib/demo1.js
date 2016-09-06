d43236a2b240cd0e35036828237af049c97da697
taoxj <taoxj1992@163.com>
2016-09-06 21:12:58 +0800

    es6

--- study/es6/lib/demo1.js
+++ study/es6/lib/demo1.js
@@ -1,4 +1,34 @@
-"use strict";
+'use strict';
 
-var str = "\n<ul>\n  <li>first</li>\n  <li>second</li>\n</ul>\n";
-console.log(str);
+var _templateObject = _taggedTemplateLiteral(['\n  <ul>\n    #for book in ', '\n      <li><i>#{book.title}</i> by #{book.author}</li>\n    #end\n  </ul>\n'], ['\n  <ul>\n    #for book in ', '\n      <li><i>#{book.title}</i> by #{book.author}</li>\n    #end\n  </ul>\n']);
+
+function _taggedTemplateLiteral(strings, raw) { return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
+
+var myBooks = [{
+	title: '说文',
+	author: '解字'
+}, {
+	title: '语文',
+	author: '李文强'
+}, {
+	title: '数学',
+	author: '刘金生'
+}, {
+	title: '导师',
+	author: '恶心'
+}];
+function hashTemplate(templateData) {
+	var s = templateData[0];
+	for (var i = 1; i < arguments.length; i++) {
+		var arg = String(arguments[i]);
+		// Escape special characters in the substitution.
+
+		s += arg.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+
+		// Don't escape special characters in the template.
+		s += templateData[i];
+	}
+	return s;
+}
+var libraryHtml = hashTemplate(_templateObject, myBooks);
+console.log(libraryHtml);
--- study/es6/src/demo1.js
+++ study/es6/src/demo1.js
@@ -1,7 +1,41 @@
-var str=`
-<ul>
-  <li>first</li>
-  <li>second</li>
-</ul>
+var myBooks=[
+		{
+			title:'说文',
+			author:'解字'
+		},
+		{
+			title:'语文',
+			author:'李文强'
+		},
+		{
+			title:'数学',
+			author:'刘金生'
+		},
+		{
+			title:'导师',
+			author:'恶心'
+		}
+	];
+function hashTemplate(templateData){
+	var s = templateData[0];
+	for (var i = 1; i < arguments.length; i++) {
+	var arg = String(arguments[i]);
+	// Escape special characters in the substitution.
+	
+	s += arg.replace(/&/g, "&amp;")
+	        .replace(/</g, "&lt;")
+	        .replace(/>/g, "&gt;");
+
+	// Don't escape special characters in the template.
+	s += templateData[i];
+	}
+	return s;
+}
+var libraryHtml = hashTemplate`
+  <ul>
+    #for book in ${myBooks}
+      <li><i>#{book.title}</i> by #{book.author}</li>
+    #end
+  </ul>
 `;
-console.log(str)
+console.log(libraryHtml);

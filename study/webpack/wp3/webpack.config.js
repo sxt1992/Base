848faf93cdeb42e6bebee71a59b3162182f541f5
taoxj <taoxj1992@163.com>
2017-01-24 20:31:19 +0800

    webpack

---
+++ study/webpack/wp3/webpack.config.js
@@ -0,0 +1,5 @@
+// https://segmentfault.com/a/1190000005866410
+// http://blog.csdn.net/github_26672553/article/details/52280655
+var validate = require('webpack-validator');
+var config = {};
+module.exports = validate(config);

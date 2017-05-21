531b23bc46396fd274b4cbb244755e352f95c784
marktao <taoxj1992@163.com>
2017-05-22 00:54:12 +0800

    redux-router

---
+++ study/reactNative/rn1/src/app.js
@@ -0,0 +1,8 @@
+import './sass/a.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import './react/redux1';
+// import './react/redux2';
+// import './react/redux3';
+import './react/redux31';
--- study/redux/rx0502/src/react/redux31.jsx
+++ study/redux/rx0502/src/react/redux31.jsx
@@ -1,15 +1,12 @@
 // React-Router 路由库 31
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider, connect } from 'react-redux';
-import { Router, Route} from 'react-router';
-import {createHashHistory } from 'history';
-
-var hashHistory = createHashHistory();
+import {Router,Route,IndexRoute,Link,IndexLink,hashHistory,browserHistory} from 'react-router';
 
 // React component
-class Counter extends Component {
+/*class Counter extends Component {
   render() {
     const { value, onIncreaseClick } = this.props
     return (
@@ -56,13 +53,62 @@ function mapDispatchToProps(dispatch) {
 const App = connect(
   mapStateToProps,
   mapDispatchToProps
-)(Counter)
+)(Counter)*/
+
+class ZongDe extends Component {
+  render() {
+    return (
+      <div>
+        ZongDe 真 DFfe <Link to="/aa">to AA</Link> <br/>
+        {this.props.children}
+        ZongDe 真 DFe <Link to="/bb">to BB</Link> <br/>
+      </div>
+    )
+  }
+}
+class AA extends Component {
+  render() {
+    return (
+      <div>
+        AAAAAAAAAAAA <br/>
+      </div>
+    )
+  }
+}
+class BB extends Component {
+  render() {
+    return (
+      <div>
+        BBBBBBBBBBBB <br/>
+        <IndexLink to="/" >to Home</IndexLink>
+      </div>
+    )
+  }
+}
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        HHHHHHHHHHHHHHH <br/>
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
-    <Provider store={store}>
-      <Router history={hashHistory}>
-        <Route path="/" component={App}/>
-      </Router>
-    </Provider>,
+    <Router history={browserHistory}>
+      <Route path="/" component={ZongDe} >
+        <IndexRoute component={Home} />
+        <Route path="/aa" component={AA}
+        onEnter={
+          function(){
+            window.tao=arguments[0];
+            console.log(arguments[0]);
+          }
+        } 
+        />
+        <Route path="/bb" component={BB} />
+      </Route>
+    </Router>,
     document.getElementById('wrapper')
 );

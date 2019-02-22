44c7ec2b315638f2db4095943007325a4f92c74b
sxt1992 <taoxj1992@163.com>
2019-02-22 17:10:09 +0800

    mockServer modify

--- study/mockServer/app.js
+++ study/mockServer/app.js
@@ -1,5 +1,30 @@
-let express = require('express');
+const express = require('express');
+const request = require('request');
+const bodyParser = require('body-parser');
+const multiparty = require('connect-multiparty');
+
 let mockData = require('./mockData.js');
+
+const ajax = (url, method, qs = {}, form = {}) => new Promise(function(resolve, reject) {
+    request({
+        url: `http://test.guanghui.dasouche.net${url}`,
+        method,
+        qs,
+        form
+    }, function(error, res, body) {
+        if (!error && (res.statusCode >= 200 && res.statusCode < 300 || res.statusCode == 304)) {
+            resolve(body);
+        } else {
+            reject(body);
+        }
+    });
+});
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(multiparty());
+
 /*
 {
   flag: true,
@@ -8,7 +33,6 @@ let mockData = require('./mockData.js');
   data: item.d()
 }
 */
-let app = express();
 
 // 允许跨域
 app.use(function(req, res, next) {
@@ -17,6 +41,12 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Headers', 'X-Requested-With');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     res.header('Content-Type', 'application/json;charset=utf-8');
+
+    // ajax(req.url, req.method, req.query , req.body).then(d => {
+    //     res.end(d);
+    //     next();
+    // }).catch(err => next());
+
     next();
 });
 
@@ -36,9 +66,9 @@ mockData.forEach(item => {
 app.all('*', function(req, res){
     res.send(JSON.stringify({success: false, status: '-1', errMsg: '接口不存在', msg: '接口不存在', data: {}}));
 });
-let server = app.listen(9290, function () {
-  let host = server.address().address;
-  let port = server.address().port;
+const server = app.listen(9290, function () {
+  const host = server.address().address;
+  const port = server.address().port;
 
   console.log('App listening at http://%s:%s', host, port);
 });
